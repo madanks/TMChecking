c69eb1f6f0de4c04fa846852f259808a0802ddc1
Madan <madan.siwakoti809@gmail.com>
2016-05-14 09:01:03 -0500

    Signed-off-by: Madan <madan.siwakoti809@gmail.com>

--- src/main/java/com/cs544/mum/repository/AppointmentRepository.java
+++ src/main/java/com/cs544/mum/repository/AppointmentRepository.java
@@ -2,6 +2,7 @@ package com.cs544.mum.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 import com.cs544.mum.domain.Appointment;
 
 public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
--- src/main/java/com/cs544/mum/serviceImpl/AppointmentServiceImpl.java
+++ src/main/java/com/cs544/mum/serviceImpl/AppointmentServiceImpl.java
@@ -3,6 +3,7 @@ package com.cs544.mum.serviceImpl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,8 +11,8 @@ import com.cs544.mum.domain.Appointment;
 import com.cs544.mum.repository.AppointmentRepository;
 import com.cs544.mum.service.AppointmentService;
 
-@Service
 @Transactional
+@Repository
 public class AppointmentServiceImpl implements AppointmentService {
 	@Autowired
 	private AppointmentRepository appointmentrepo;
