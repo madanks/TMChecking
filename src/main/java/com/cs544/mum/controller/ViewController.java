2f470d33f01be714904dce9745170a857b027400
AsmaaAlshal <asmaa.elshal.abdelfatah@gmail.com>
2016-05-14 21:19:36 -0500

    Testing  Spring Security

--- src/main/java/com/cs544/mum/controller/ViewController.java
+++ src/main/java/com/cs544/mum/controller/ViewController.java
@@ -2,6 +2,7 @@ package com.cs544.mum.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // Commiting 
 @Controller
@@ -10,5 +11,14 @@ public class ViewController {
 	public String goToHomePage() {
 		return "home";
 	}
+	@RequestMapping("/home")
+	public String goToHomePag() {
+		return "test";
+	}
 
+	@RequestMapping(value="/admin/home" , method = RequestMethod.GET)
+	public String  privateHome ()
+	{
+		return "adminHome" ; 
+	}
 }
